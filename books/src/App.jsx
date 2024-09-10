42882506eeedb23e615707bbec7ae4c3b2e9d79e
Alessandro-Gomes-Bravio <9021764@student.zadkine.nl>
2024-09-10 13:14:52 +0200

    Initial commit

---
+++ books/src/App.jsx
@@ -0,0 +1,13 @@
+
+import Booklist from "./components/Booklist"
+
+function App() {
+  return (
+    <>
+      <h1>nnnnnnnnnnnnnnnnnnnnnnn</h1>
+      <Booklist/>
+    </>
+  )
+}
+
+export default App
